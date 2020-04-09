e612818645d88b7bdb4246b3b657fb5d4da32cad
Isabelle <sutti554@newschool.edu>
2020-04-09 13:37:31 -0400

    combining laptop and phone files

---
+++ studio/SessionK-Studio/Week11-Class02/combining-them/script.js
@@ -0,0 +1,33 @@
+/*-------Laptop JS-----------*/
+
+document.getElementById("fading").onclick = function () {
+  document.getElementById("dream").style.color = "rgb(228, 1, 1)";
+};
+
+document.getElementById("dream").onclick = function () {
+  document.getElementById("ready").style.color = "rgb(228, 1, 1)";
+};
+
+document.getElementById("ready").onclick = function () {
+  document.getElementById("for").style.color = "rgb(228, 1, 1)";
+};
+
+document.getElementById("for").onclick = function () {
+  document.getElementById("nerve").style.color = "rgb(228, 1, 1)";
+};
+
+document.getElementById("nerve").onclick = function () {
+  document.getElementById("fading").style.color = "rgb(228, 1, 1)";
+  document.getElementById("dream").style.color = "black";
+  document.getElementById("ready").style.color = "black";
+  document.getElementById("for").style.color = "black";
+  document.getElementById("nerve").style.color = "black";
+};
+
+/* ----------- iPhone 6, 6S, 7 and 8 ----------- */
+
+document.getElementById("container2").onclick = function () {
+  document.getElementById("everything").style.color = "rgb(228, 1, 1)";
+  document.getElementById("flickers").style.color = "rgb(228, 1, 1)";
+  document.getElementById("real").style.color = "rgb(228, 1, 1)";
+};
---
+++ studio/SessionK-Studio/Week11-Class02/phone-trial/script.js
@@ -0,0 +1,7 @@
+/* ----------- iPhone 6, 6S, 7 and 8 ----------- */
+
+document.getElementById("container").onclick = function () {
+  document.getElementById("everything").style.color = "rgb(228, 1, 1)";
+  document.getElementById("flickers").style.color = "rgb(228, 1, 1)";
+  document.getElementById("real").style.color = "rgb(228, 1, 1)";
+};
