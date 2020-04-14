96d38d7f4353813ab75bbfe956e98ea02db49fbf
Isabelle <sutti554@newschool.edu>
2020-04-14 13:25:14 -0400

    highlight

---
+++ studio/SessionK-Studio/Week12-Class01/HW/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('#container').mousemove(function (e) {
+        var mousePosX = (e.pageX / $(window).width()) * 100;
+        $('#container').css('background-position-x', mousePosX + '%');
+        var mousePosY = (e.pageY / $(window).height()) * 100;
+        $('#container').css('background-position-y', mousePosY + '%');
+        console.log(mousePosX, mousePosY);
+    });
+});
