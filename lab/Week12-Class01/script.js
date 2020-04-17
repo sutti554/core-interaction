fa074f75e38f0a5353183d7576e580716e960308
Isabelle <sutti554@newschool.edu>
2020-04-16 21:35:08 -0400

    lab hw and homepage update

---
+++ lab/Week12-Class01/script.js
@@ -0,0 +1,11 @@
+if (window.innerWidth < 700) {
+    document.getElementById("container").style.backgroundColor = "pink";
+} else {
+    document.getElementById("container").style.backgroundColor = "rgb(235, 88, 113)";
+}
+
+if (window.innerWidth > 1000) {
+    document.getElementById("container").style.backgroundColor = "red";
+}
+
+
