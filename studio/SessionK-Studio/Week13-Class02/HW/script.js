946923974d57768ff1912ab14cdab8e3d9c038c6
Isabelle <sutti554@newschool.edu>
2020-04-23 13:38:32 -0400

    change to laptop version

--- studio/SessionK-Studio/Week13-Class02/HW/script.js
+++ studio/SessionK-Studio/Week13-Class02/HW/script.js
@@ -1,13 +1,3 @@
-$(document).ready(function () {
-    $('#container').mousemove(function (e) {
-        var mousePosX = (e.pageX / $(window).width()) * 100;
-        $('#container').css('background-position-x', mousePosX + '%');
-        var mousePosY = (e.pageY / $(window).height()) * 100;
-        $('#container').css('background-position-y', mousePosY + '%');
-        console.log(mousePosX, mousePosY);
-    });
-});
-
 
 function playAudio(url) {
     new Audio(url).play();
